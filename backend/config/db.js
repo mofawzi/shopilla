5d67fb19fe2c64e2c6902262d6dd059d35d6d51c
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-12-19 04:46:29 +0200

    Add colors package

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,9 +10,9 @@ const connectDB = async () => {
       useCreateIndex: true,
     });
 
-    console.log(`MongoDB Connected: ${con.connection.host}`);
+    console.log(`MongoDB Connected: ${con.connection.host}`.cyan.underline);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`.underline.bold);
     process.exit(1);
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
+import colors from "colors";
 import connectDB from "./config/db.js";
 
+// Access environment variables
 dotenv.config();
 
+// Connect to database middleware (Mongoose)
 connectDB();
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello"));
+app.get("/", (req, res) => res.send("API is running... "));
 
 // Get all products
 app.get("/api/products", (req, res) => {
@@ -27,6 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode, on Port: ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode, on Port: ${PORT}`.yellow
+      .bold
   )
 );
