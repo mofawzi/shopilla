4ba9f604987f70a92a8ed1b197c6ffbc3d2c8c21
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-12-21 19:14:19 +0200

    Add dummy users

---
+++ backend/data/users.js
@@ -0,0 +1,20 @@
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Mohamed Fawzy",
+    email: "mohamed@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Ahmed Alaa",
+    email: "ahmed@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default users;
