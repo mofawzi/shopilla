bd1536cfc4f2820fa21bf477d10081e33312ae17
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-12-24 00:16:48 +0200

    Update cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Row,
+  Col,
+  ListFroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import Message from "../components/Message";
+import { addToCart } from "../actions/cartActions";
 
-const CartScreen = () => {
+// Distructure props
+const CartScreen = ({ match, location, history }) => {
+  // Get product id from URL
+  const productId = match.params.id;
+
+  // Get quantity from URL ( ex. qty=1)
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return <div>Cart</div>;
 };
 
