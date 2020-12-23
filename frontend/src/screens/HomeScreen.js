1bc1d513d868c2671de530265107a93736a2d005
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-12-23 07:12:13 +0200

    Apply redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,33 +1,43 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-  // Make products a component level state
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
 
+  // Get the data of the store (state)
+  const productList = useSelector((state) => state.productList);
+
+  const { loading, error, products } = productList;
+
+  // Firing the action
   useEffect(() => {
-    const fetchProducts = async () => {
-      // getting data from response object
-      const { data } = await axios.get("/api/products");
-
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {
+        // Check if loading
+        loading ? (
+          <h2>Loading...</h2>
+        ) : // Check if errors
+        error ? (
+          <h3>{error}</h3>
+        ) : (
+          // Show Products
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        )
+      }
     </>
   );
 };
