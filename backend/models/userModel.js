fbc0bb92f87d8e5aff8f667995e7fdfd459e1fcd
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-12-25 01:56:52 +0200

    Add password Encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -33,6 +33,19 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Encrypt password before saving
+userSchema.pre("save", async function (next) {
+  // Only happens when creating a new password or when modifying
+  if (!this.isModified("password")) {
+    // if modifying other data(name, email, ...)
+    next();
+  }
+  // Add salt for hashing
+  const salt = await bcrypt.genSalt(10);
+  // reset password to be a hashed one
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
