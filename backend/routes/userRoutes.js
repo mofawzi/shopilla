b81dcfe77dcd779c2b08e995988e509d439e2f12
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-12-25 01:06:24 +0200

    Add get user profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { authUser } from "../controllers/userController.js";
+import { authUser, getUserProfile } from "../controllers/userController.js";
 
 const router = express.Router();
 
 // @desc    Authenticate user
 router.post("/login", authUser);
+// @desc    Get user profile
+router.route("/profile").get(getUserProfile);
 
 export default router;
