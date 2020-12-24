82ecc3dd2958ce8250c35047221f0c1980ef77da
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-12-24 03:35:08 +0200

    Remove item from cart

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`);
@@ -16,6 +16,16 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
     },
   });
 
-  // Save to local storage to be added to store
+  // Save to local state
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  });
+
+  // Update local state
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
