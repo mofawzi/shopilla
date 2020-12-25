9bc741ce32d612d1e64aa509040d15655e1fa4f3
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-12-25 23:41:01 +0200

    Add user register action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -4,6 +4,9 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
@@ -55,4 +58,52 @@ export const logout = () => async (dispatch) => {
   dispatch({
     type: USER_LOGOUT,
   });
+};
+
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    // Dispatch request action
+    dispatch({
+      type: USER_REGISTER_REQUEST,
+    });
+
+    // Send content type in the header
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    // Make register request and destructure the data
+    const { data } = await axios.post(
+      "/api/users",
+      { name, email, password },
+      config
+    );
+
+    // Dispatch the success register action
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    });
+
+    // Auto login after a successful registeration
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    // Save user data to local storage
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    // Failed to login
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      // First check for the error message in the backend
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
