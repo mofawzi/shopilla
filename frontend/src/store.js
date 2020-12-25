5d831517a5310e7010416130d0950389e69dcc3d
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-12-25 21:18:33 +0200

    Add user login reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,12 +6,14 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 // Main Reducer
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 // Get cart items from local storage
