49f49f20a4ed3296ddb00b2a8b132292eba2ad72
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-12-29 08:57:43 +0200

    Add - Save shipping address

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -34,8 +34,16 @@ const userInfoFromStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
   : null;
 
+// Get shipping address from local storage
+const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : {};
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
   userLogin: { userInfo: userInfoFromStorage },
 };
 
