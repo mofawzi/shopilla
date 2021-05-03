97b77bf7e7c9a453c198ae497b7e6cc2ea70a8b5
Mohamed Fawzy <mofawzy1996@gmail.com>
2021-05-03 15:23:47 +0200

    Updated Placeorder Step

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -38,7 +38,7 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
 
       <Nav.Item>
         {step4 ? (
-          <LinkContainer to="/placeOrder">
+          <LinkContainer to="/placeorder">
             <Nav.Link>Place Order</Nav.Link>
           </LinkContainer>
         ) : (
