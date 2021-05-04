71e5992b45c01af904ad1ed33ec5ed44d7f4e863
Mohamed Fawzy <mofawzy1996@gmail.com>
2021-05-04 11:55:39 +0200

    Add create order Reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,6 +12,7 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from "./reducers/userReducers";
+import { orderCreateReducer, orderReducer } from "./reducers/orderReducers";
 
 // Main Reducer
 const reducer = combineReducers({
@@ -22,6 +23,7 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  orderCreate: orderCreateReducer,
 });
 
 // Get cart items from local storage
