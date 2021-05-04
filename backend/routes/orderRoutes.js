6512a1737ced6f7d3a97cb813113e626ea0ebc79
Mohamed Fawzy <mofawzy1996@gmail.com>
2021-05-04 13:29:19 +0200

    Add - get order by ID

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { addOrderItems } from "../controllers/orderController.js";
+import { addOrderItems, getOrderById } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // @desc    Add order items
 router.route("/").post(protect, addOrderItems);
+
+// @desc    Get order by ID
+router.route("/:id").get(protect, getOrderById);
 
 export default router;
