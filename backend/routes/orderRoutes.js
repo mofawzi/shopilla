5c378ab776c62728d1786b7b21fc67515211ecb8
Mohamed Fawzy <mofawzy1996@gmail.com>
2021-05-09 02:35:33 +0200

    Add - Get user orders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,6 +3,7 @@ import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  getMyOrders,
 } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -11,8 +12,13 @@ const router = express.Router();
 // @desc    Add order items
 router.route("/").post(protect, addOrderItems);
 
+// @desc    Get order of the logged in user
+router.route("/myorders").get(protect, getMyOrders);
+
 // @desc    Get order by ID
 router.route("/:id").get(protect, getOrderById);
+
+// @desc    Pay an order
 router.route("/:id/pay").put(protect, updateOrderToPaid);
 
 export default router;
