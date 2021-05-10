cb6fcbeb04a289b12ecadc933bf1c1c4bcc49526
Mohamed Fawzy <mofawzy1996@gmail.com>
2021-05-10 16:41:42 +0200

    Add - Delete Product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,14 +2,16 @@ import express from "express";
 import {
   getProducts,
   getProductById,
+  deleteProduct,
 } from "../controllers/productController.js";
+
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // @desc    Fetch all products
 router.route("/").get(getProducts);
 
-// @desc    Fetch single product
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
