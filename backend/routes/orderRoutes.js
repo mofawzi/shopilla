e5d4d8e76d90d1b333a3df049f040c9a545bd109
Mohamed Fawzy <mofawzy1996@gmail.com>
2021-05-11 15:21:29 +0200

    Add - Admin get orders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,13 +4,14 @@ import {
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
+  getOrders,
 } from "../controllers/orderController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // @desc    Add order items
-router.route("/").post(protect, addOrderItems);
+router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
 
 // @desc    Get order of the logged in user
 router.route("/myorders").get(protect, getMyOrders);
