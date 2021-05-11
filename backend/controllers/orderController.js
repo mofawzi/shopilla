21900fffa1d9e8b0d7c0974d4dc5d5ac71325f9a
Mohamed Fawzy <mofawzy1996@gmail.com>
2021-05-11 16:07:46 +0200

    Add - Admin order deliver endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -55,7 +55,7 @@ const getOrderById = asyncHandler(async (req, res) => {
 });
 
 // @desc    Update order to be paid
-// @route   GET /api/orders/:id/pay
+// @route   PUT /api/orders/:id/pay
 // @access  Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
   const order = await Order.findById(req.params.id);
@@ -70,6 +70,24 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
       update_time: req.body.update_time,
       email_address: req.body.payer.email_address,
     };
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+// @desc    Update order to be delivered
+// @route   PUT /api/orders/:id/deliver
+// @access  Private/Admin
+const updateOrderToDeliverd = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
     const updatedOrder = await order.save();
     res.json(updatedOrder);
   } else {
@@ -100,6 +118,7 @@ export {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  updateOrderToDeliverd,
   getMyOrders,
   getOrders,
 };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,6 +3,7 @@ import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  updateOrderToDeliverd,
   getMyOrders,
   getOrders,
 } from "../controllers/orderController.js";
@@ -10,16 +11,14 @@ import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// @desc    Add order items
 router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
-
 // @desc    Get order of the logged in user
 router.route("/myorders").get(protect, getMyOrders);
-
 // @desc    Get order by ID
 router.route("/:id").get(protect, getOrderById);
-
 // @desc    Pay an order
 router.route("/:id/pay").put(protect, updateOrderToPaid);
+// @desc    Deliver an order
+router.route("/:id/deliver").put(protect, admin, updateOrderToDeliverd);
 
 export default router;
